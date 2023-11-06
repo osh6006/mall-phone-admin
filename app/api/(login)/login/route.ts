4f63bc0f73e3932bf3d34fb4e068205b3a660afb
OHS <ohs6006@gmail.com>
2023-11-06 12:10:15 +0900

    ADD : 로그인 JWT 로직 추가

--- app/api/(login)/login/route.ts
+++ app/api/(login)/login/route.ts
@@ -1,5 +1,19 @@
+import * as bcrypt from "bcrypt";
+
+import { NextResponse } from "next/server";
+
 import prismadb from "@/lib/prismadb";
-import { customFailNextResponse, customSuccessNextResponse } from "@/lib/utils";
+import { signJwtAccessToken } from "@/lib/jwt";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
 
 export async function POST(request: Request) {
   try {
@@ -8,11 +22,21 @@ export async function POST(request: Request) {
     const { email, password } = body;
 
     if (!email) {
-      return customFailNextResponse("이메일이 필요합니다.", 400);
+      return NextResponse.json(
+        { ok: false, msg: "이메일을 입력해 주세요" },
+        {
+          headers: corsHeaders,
+        }
+      );
     }
 
     if (!password) {
-      return customFailNextResponse("비밀번호가 필요합니다.", 400);
+      return NextResponse.json(
+        { ok: false, msg: "비밀번호를 입력해 주세요" },
+        {
+          headers: corsHeaders,
+        }
+      );
     }
 
     const user = await prismadb.user.findUnique({
@@ -22,12 +46,47 @@ export async function POST(request: Request) {
     });
 
     if (!user) {
-      return customFailNextResponse("유저가 존재하지 않습니다.", 403);
+      return NextResponse.json(
+        { ok: false, msg: "비밀번호 혹은 이메일이 일치하지 않습니다." },
+        {
+          headers: corsHeaders,
+        }
+      );
     }
 
-    return customSuccessNextResponse(user);
+    const isCorrect = await bcrypt.compare(body.password, user.password);
+
+    if (!isCorrect) {
+      return NextResponse.json(
+        { ok: false, msg: "비밀번호 혹은 이메일이 일치하지 않습니다." },
+        {
+          headers: corsHeaders,
+        }
+      );
+    }
+
+    const { password: tempPassword, ...userWithoutPass } = user;
+
+    // 추가된 부분
+    const accessToken = signJwtAccessToken(userWithoutPass);
+    const result = {
+      ...userWithoutPass,
+      accessToken,
+    };
+
+    return NextResponse.json(
+      { ok: true, msg: "로그인에 성공하셨습니다.", result },
+      {
+        headers: corsHeaders,
+      }
+    );
   } catch (error) {
     console.log("[LOGIN_POST]", error);
-    return customFailNextResponse("서버에서 오류가 발생했습니다.", 500);
+    return NextResponse.json(
+      { ok: false, msg: "서버에서 오류가 발생하였습니다." },
+      {
+        headers: corsHeaders,
+      }
+    );
   }
 }
---
+++ lib/jwt.ts
@@ -0,0 +1,18 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface SignOption {
+  expiresIn?: string | number;
+}
+
+const DEFAULT_SIGN_OPTION: SignOption = {
+  expiresIn: "1h",
+};
+
+export function signJwtAccessToken(
+  payload: JwtPayload,
+  options: SignOption = DEFAULT_SIGN_OPTION
+) {
+  const secret_key = process.env.SECRET_KEY;
+  const token = jwt.sign(payload, secret_key!, options);
+  return token;
+}
